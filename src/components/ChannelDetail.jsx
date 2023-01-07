cfaa17ae7e5d97de96c1f030aa8794f4465c9fdb
Harout Terterian <hyecartoon@gmail.com>
2023-01-06 23:54:18 -0800

    Project Done - All components functional

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -6,15 +6,35 @@ import { Videos, ChannelCard } from './';
 import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState(null)
+  const [videos, setVideos] = useState([])
+
   const { id } = useParams();
 
   useEffect(() => {
-    fetchFromAPI(`channels?part='snippet&id=${id}'`)
+    fetchFromAPI(`channels?part=snippet&id=${id}`)
+      .then((data) => setChannelDetail(data?.items[0]));
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
+      .then((data) => setVideos(data?.items));
   }, [id])
+
   return (
-    <div>
-      
-    </div>
+    <Box minHeight='95vh'>
+      <Box>
+        <div
+          style={{
+            background: 'linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(5,77,20,1) 35%, rgba(255,159,28,1) 100%)',
+            zIndex: 10,
+            height: '300px'
+          }}
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop="-125px" />
+      </Box>
+      <Box display='flex' p='2'>
+        <Box sx={{ mr: { sm: '100px' } }} />
+        <Videos videos={videos} />
+      </Box>
+    </Box>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { Stack } from '@mui/material';
+import { Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { logo } from '../utils/constants';
 import SearchBar from './SearchBar';
@@ -10,7 +11,11 @@ const Navbar = () => (
         p={2}
         sx={{ position: 'sticky', background: '#000', top: 0, justifyContent: 'space-between' }}>
         <Link to='/' style={{ display: 'flex', alignItems: 'center' }}>
+        
             <img src={logo} alt='logo' height={45} />
+            <Typography variant='h4' fontWeight='bold' sx={{ color: 'white' }}>
+          arouTube
+        </Typography>
         </Link>
         <SearchBar />
     </Stack>
