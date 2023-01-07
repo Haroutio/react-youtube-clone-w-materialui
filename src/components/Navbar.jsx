edaa1b94c025c4f494ef508315561dc2e097209e
Harout Terterian <hyecartoon@gmail.com>
2023-01-06 23:56:27 -0800

    Formatted

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,11 +11,11 @@ const Navbar = () => (
         p={2}
         sx={{ position: 'sticky', background: '#000', top: 0, justifyContent: 'space-between' }}>
         <Link to='/' style={{ display: 'flex', alignItems: 'center' }}>
-        
+
             <img src={logo} alt='logo' height={45} />
             <Typography variant='h4' fontWeight='bold' sx={{ color: 'white' }}>
-          arouTube
-        </Typography>
+                arouTube
+            </Typography>
         </Link>
         <SearchBar />
     </Stack>
